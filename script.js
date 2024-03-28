07f47526d6d7d709150cd94880c8a35bbac1314d
Sanelma B <92818820+jackiebeeh@users.noreply.github.com>
2024-03-28 17:19:33 +0200

    Added rule into js file to fix bug with the arrow button function.
    Now the arrow button rules are only read if user is on the index page.

--- script.js
+++ script.js
@@ -4,23 +4,25 @@ let pages = document.querySelectorAll(".pages");
 let frontPage = document.querySelector(".frontPage");
 let currentScroll = window.scrollY;
 
-arrow.addEventListener("click", () => {
-    if (window.scrollY === 0 || window.scrollY < 600) {
-        window.scroll({
-            top: 600,
-            left: 0,
-            behavior: "smooth",
-        });
-    }
-    else {
-        window.scroll({
-            top: window.scrollY + 760,
-            left: 0,
-            behavior: "smooth",
-        });
-    }
-    currentScroll = window.scrollY;
-})
+if (document.title === "Johannes") {
+    arrow.addEventListener("click", () => {
+        if (window.scrollY === 0 || window.scrollY < 600) {
+            window.scroll({
+                top: 600,
+                left: 0,
+                behavior: "smooth",
+            });
+        }
+        else {
+            window.scroll({
+                top: window.scrollY + 760,
+                left: 0,
+                behavior: "smooth",
+            });
+        }
+        currentScroll = window.scrollY;
+    })
+}
 
 // Window on load load events:
 let loader = document.getElementById("loader-page");
